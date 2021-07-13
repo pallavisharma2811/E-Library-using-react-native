5e71a3e634c2cac6a17064f28e51a57420de6858
Pallavi User <pallavisharma2811@gmail.com>
2021-07-13 15:18:03 +0530

    bottomTab navigation

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import TransactionScreen from './screens/TransactionsScreen';
+import SearchScreen from './screens/SearchScreen';
+
+export default class App extends React.Component {
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AppContainer />
     </View>
   );
+  }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  Transactions: {screen: TransactionScreen},
+  Search: {screen: SearchScreen}
+})
+
+const AppContainer = createAppContainer(TabNavigator)
 
 const styles = StyleSheet.create({
   container: {
